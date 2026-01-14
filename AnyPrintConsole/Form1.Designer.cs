6405246af267ed8e128a59644467be9756334a14
rheza.ecaldy <rheza.ecaldy@gopay.co.id>
2026-01-14 23:48:16 +0700

    Initial commit

---
+++ AnyPrintConsole/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace AnyPrintConsole
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(363, 133);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "i64mom";
+            this.textBox1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(1453, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GET FILE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(1453, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 60);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "PRINT";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(577, 239);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(835, 53);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(577, 119);
+            this.textBox2.MaxLength = 6;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(835, 60);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(577, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 31);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Enter Anyprint Code:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(571, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 31);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Your File:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(561, 356);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(266, 273);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1905, 1025);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "AnyPrint Console";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AnyPrintConsole/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Pdf;
+
+using System.Runtime.InteropServices;
+
+
+namespace AnyPrintConsole
+{
+    public partial class Form1 : Form
+
+
+    {
+        private Process onScreenKeyboardproc;
+        string ffile;
+        int codelenght;
+        string filePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string[] files = Directory.GetFiles("C:/AnyPrintFolder/FilesToPrint");
+            //foreach (string file in files) 
+            //{
+            //    string filename = Path.GetFileName(file); ;
+            //    textBox3.Text = textBox3.Text +"\r\n"+ filename; 
+            //}
+          
+            
+            textBox4.Text = "";
+
+
+            string code6 = textBox2.Text;
+           codelenght = code6.Length; 
+
+            
+
+            if ( codelenght == 6)
+            {
+                //string[] files = Directory.GetFiles("C:/AnyPrintFolder/FilesToPrint", textBox1.Text  + "*.pdf");
+                string[] files = Directory.GetFiles("C:/AnyPrintFolder/FilesToPrint", textBox2.Text + "*.pdf");
+                foreach (string file in files)
+                {
+                    string filename = Path.GetFileName(file); ;
+                    //ffile = "\r\n"+ filename; 
+                    ffile = filename;
+
+                }
+              
+            }
+           
+
+
+            if (ffile != null)
+                            {
+                filePath = @"C:/AnyPrintFolder/FilesToPrint/" + ffile;
+                textBox4.Text = ffile.Remove(0, 7);
+                textBox4.Select();
+                               
+            }
+            else
+
+            {
+                MessageBox.Show("The Print Code is NOT VALID");
+            }
+
+        }
+
+        private void CallKeyboard()
+        {
+            // TRo Close All Open On Screen ketboard
+            Process[] oslProcessesArry = Process.GetProcessesByName("TabTip");
+            foreach (Process onScreenProcess in oslProcessesArry)
+            {
+                onScreenProcess.Kill();
+            }
+
+            //To Open keyboard
+
+            string progFiles = @"C:\Program Files\Common Files\microsoft shared\ink";
+            string onScreenKeyboardPath = System.IO.Path.Combine(progFiles, "TabTip.exe");
+            onScreenKeyboardproc = System.Diagnostics.Process.Start(onScreenKeyboardPath);
+        }
+
+       
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            CallKeyboard();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(230, 226, 223);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text !="")
+            {             //Create PdfDocument object
+                PdfDocument pdf = new PdfDocument();
+
+                //Load a PDF file
+                //pdf.LoadFromFile(@"C:\AnyPrintFolder\FilesToPrint\PDFCopy.pdf");
+                //Directory.GetFiles("C:/AnyPrintFolder/FilesToPrint", textBox1.Text + "*.pdf");
+                pdf.LoadFromFile(@"C:\AnyPrintFolder\FilesToPrint\" + ffile + "");
+
+
+                //Print with default printer 
+                pdf.Print();
+
+                textBox2.Text = "";
+                textBox4.Text = "";
+
+
+
+               
+
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                    //Console.WriteLine($"File '{filePath}' deleted successfully.");
+                }
+
+                ffile = null;
+            }
+          
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            Process[] oslProcessesArry = Process.GetProcessesByName("TabTip");
+            foreach (Process onScreenProcess in oslProcessesArry)
+            {
+                onScreenProcess.Kill();
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (System.Diagnostics.Process onProcess in System.Diagnostics.Process.GetProcessesByName("TabTip")) 
+            {
+                onProcess.Kill();
+            }
+        }
+
+       
+    }
+}
