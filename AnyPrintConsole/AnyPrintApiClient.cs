301906da166bda278d88f010ac28f7008dd011bc
rheza.ecaldy <rheza.ecaldy@gopay.co.id>
2026-01-15 10:13:28 +0700

    feat : download file from minIO

---
+++ AnyPrintConsole/AnyPrintApiClient.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace AnyPrintConsole
+{
+    public class AnyPrintJob
+    {
+        public string code { get; set; }
+        public string filename { get; set; }
+        public string fileUrl { get; set; }
+        public int copies { get; set; }
+        public int pages { get; set; }
+    }
+
+    public class AnyPrintApiClient
+    {
+        private const string BaseUrl = "https://anyprint.id/api";
+
+        public AnyPrintJob GetJob(string code)
+        {
+            var url = $"{BaseUrl}/jobs/{code}";
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Timeout = 15000;
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            using (var reader = new StreamReader(stream))
+            {
+                var json = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<AnyPrintJob>(json);
+            }
+        }
+
+        public string DownloadFile(string fileUrl, string saveFolder)
+        {
+            var fileName = Path.GetFileName(fileUrl);
+            var localPath = Path.Combine(saveFolder, fileName);
+
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(fileUrl, localPath);
+            }
+
+            return localPath;
+        }
+    }
+}
--- AnyPrintConsole/Form1.cs
+++ AnyPrintConsole/Form1.cs
@@ -1,25 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows.Forms;
 using Spire.Pdf;
-
-using System.Runtime.InteropServices;
-
 
 namespace AnyPrintConsole
 {
     public partial class Form1 : Form
-
-
     {
+        private AnyPrintApiClient apiClient = new AnyPrintApiClient();
+
         private Process onScreenKeyboardproc;
         string ffile;
         int codelenght;
@@ -32,70 +24,54 @@ namespace AnyPrintConsole
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //string[] files = Directory.GetFiles("C:/AnyPrintFolder/FilesToPrint");
-            //foreach (string file in files) 
-            //{
-            //    string filename = Path.GetFileName(file); ;
-            //    textBox3.Text = textBox3.Text +"\r\n"+ filename; 
-            //}
-          
-            
             textBox4.Text = "";
+            ffile = null;
 
+            string code = textBox2.Text.Trim();
 
-            string code6 = textBox2.Text;
-           codelenght = code6.Length; 
-
-            
-
-            if ( codelenght == 6)
+            if (code.Length < 6)
             {
-                //string[] files = Directory.GetFiles("C:/AnyPrintFolder/FilesToPrint", textBox1.Text  + "*.pdf");
-                string[] files = Directory.GetFiles("C:/AnyPrintFolder/FilesToPrint", textBox2.Text + "*.pdf");
-                foreach (string file in files)
-                {
-                    string filename = Path.GetFileName(file); ;
-                    //ffile = "\r\n"+ filename; 
-                    ffile = filename;
-
-                }
-              
+                MessageBox.Show("Invalid code");
+                return;
             }
-           
 
+            try
+            {
+                var job = apiClient.GetJob(code);
 
-            if (ffile != null)
-                            {
-                filePath = @"C:/AnyPrintFolder/FilesToPrint/" + ffile;
-                textBox4.Text = ffile.Remove(0, 7);
+                string folder = @"C:\AnyPrintFolder\FilesToPrint";
+                Directory.CreateDirectory(folder);
+
+                string localFilePath = apiClient.DownloadFile(job.fileUrl, folder);
+
+                ffile = Path.GetFileName(localFilePath);
+                filePath = localFilePath;
+
+                textBox4.Text = job.filename;
                 textBox4.Select();
-                               
             }
-            else
-
+            catch (WebException)
             {
-                MessageBox.Show("The Print Code is NOT VALID");
+                MessageBox.Show("Job not found or not paid yet.");
             }
-
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to download file: " + ex.Message);
+            }
         }
 
         private void CallKeyboard()
         {
-            // TRo Close All Open On Screen ketboard
             Process[] oslProcessesArry = Process.GetProcessesByName("TabTip");
             foreach (Process onScreenProcess in oslProcessesArry)
             {
                 onScreenProcess.Kill();
             }
 
-            //To Open keyboard
-
             string progFiles = @"C:\Program Files\Common Files\microsoft shared\ink";
-            string onScreenKeyboardPath = System.IO.Path.Combine(progFiles, "TabTip.exe");
-            onScreenKeyboardproc = System.Diagnostics.Process.Start(onScreenKeyboardPath);
+            string onScreenKeyboardPath = Path.Combine(progFiles, "TabTip.exe");
+            onScreenKeyboardproc = Process.Start(onScreenKeyboardPath);
         }
-
-       
 
         private void textBox2_Enter(object sender, EventArgs e)
         {
@@ -107,38 +83,24 @@ namespace AnyPrintConsole
             BackColor = Color.FromArgb(230, 226, 223);
         }
 
-
         private void button2_Click(object sender, EventArgs e)
         {
-            if (textBox4.Text !="")
-            {             //Create PdfDocument object
+            if (textBox4.Text != "")
+            {
                 PdfDocument pdf = new PdfDocument();
-
-                //Load a PDF file
-                //pdf.LoadFromFile(@"C:\AnyPrintFolder\FilesToPrint\PDFCopy.pdf");
-                //Directory.GetFiles("C:/AnyPrintFolder/FilesToPrint", textBox1.Text + "*.pdf");
-                pdf.LoadFromFile(@"C:\AnyPrintFolder\FilesToPrint\" + ffile + "");
-
-
-                //Print with default printer 
+                pdf.LoadFromFile(@"C:\AnyPrintFolder\FilesToPrint\" + ffile);
                 pdf.Print();
 
                 textBox2.Text = "";
                 textBox4.Text = "";
 
-
-
-               
-
                 if (File.Exists(filePath))
                 {
                     File.Delete(filePath);
-                    //Console.WriteLine($"File '{filePath}' deleted successfully.");
                 }
 
                 ffile = null;
             }
-          
         }
 
         private void textBox2_Leave(object sender, EventArgs e)
@@ -148,17 +110,14 @@ namespace AnyPrintConsole
             {
                 onScreenProcess.Kill();
             }
-
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            foreach (System.Diagnostics.Process onProcess in System.Diagnostics.Process.GetProcessesByName("TabTip")) 
+            foreach (Process onProcess in Process.GetProcessesByName("TabTip"))
             {
                 onProcess.Kill();
             }
         }
-
-       
     }
 }
