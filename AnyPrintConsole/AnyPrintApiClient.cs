a35afed8982878602af2a3db275181f9d9bfad99
rheza.ecaldy <rheza.ecaldy@gopay.co.id>
2026-02-21 12:58:49 +0700

    fix : missing module

--- AnyPrintConsole/AnyPrintApiClient.cs
+++ AnyPrintConsole/AnyPrintApiClient.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Net;
 using Newtonsoft.Json;
 
 namespace AnyPrintConsole
--- AnyPrintConsole/Form1.cs
+++ AnyPrintConsole/Form1.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Windows.Forms;
 using System.Drawing.Printing;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace AnyPrintConsole
 {
