dfa64830e8ad772ebe0ebc36292c2257b504ea9d
rheza.ecaldy <rheza.ecaldy@gopay.co.id>
2026-02-21 10:51:20 +0700

    feat : button hover animation

--- AnyPrintConsole/GradientButton.cs
+++ AnyPrintConsole/GradientButton.cs
@@ -1,49 +1,98 @@
+using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 public class GradientButton : Button
 {
-  public Color Color1 { get; set; }
-  public Color Color2 { get; set; }
+    public Color Color1 { get; set; }
+    public Color Color2 { get; set; }
 
-  public GradientButton()
-  {
-      this.FlatStyle = FlatStyle.Flat;
-      this.FlatAppearance.BorderSize = 0;
-      this.BackColor = Color.Transparent;
-      this.ForeColor = Color.White;
-  }
+    private bool isHovered = false;
+    private bool isPressed = false;
 
-  protected override void OnPaint(PaintEventArgs pevent)
-  {
-    Graphics g = pevent.Graphics;
-    g.SmoothingMode = SmoothingMode.AntiAlias;
-
-    // Draw gradient background
-    using (LinearGradientBrush brush =
-      new LinearGradientBrush(this.ClientRectangle, Color1, Color2, 0f))
+    public GradientButton()
     {
-      g.FillRectangle(brush, this.ClientRectangle);
+        this.FlatStyle = FlatStyle.Flat;
+        this.FlatAppearance.BorderSize = 0;
+        this.BackColor = Color.Transparent;
+        this.ForeColor = Color.White;
+        this.DoubleBuffered = true;
+
+        this.MouseEnter += (s, e) =>
+        {
+            isHovered = true;
+            this.Invalidate();
+        };
+
+        this.MouseLeave += (s, e) =>
+        {
+            isHovered = false;
+            isPressed = false;
+            this.Invalidate();
+        };
+
+        this.MouseDown += (s, e) =>
+        {
+            isPressed = true;
+            this.Invalidate();
+        };
+
+        this.MouseUp += (s, e) =>
+        {
+            isPressed = false;
+            this.Invalidate();
+        };
     }
 
-    // Draw centered text
-    TextRenderer.DrawText(
-      g,
-      this.Text,
-      this.Font,
-      this.ClientRectangle,
-      this.ForeColor,
-      TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
-
-    // Disabled overlay
-    if (!this.Enabled)
+    protected override void OnPaint(PaintEventArgs pevent)
     {
-      using (SolidBrush overlay =
-        new SolidBrush(Color.FromArgb(120, Color.Black)))
-      {
-        g.FillRectangle(overlay, this.ClientRectangle);
-      }
+        Graphics g = pevent.Graphics;
+        g.SmoothingMode = SmoothingMode.AntiAlias;
+
+        Rectangle rect = this.ClientRectangle;
+
+        Color c1 = Color1;
+        Color c2 = Color2;
+
+        // Hover effect (slightly brighter)
+        if (isHovered && this.Enabled)
+        {
+            c1 = ControlPaint.Light(Color1, 0.2f);
+            c2 = ControlPaint.Light(Color2, 0.2f);
+        }
+
+        // Press effect (slightly darker)
+        if (isPressed && this.Enabled)
+        {
+            c1 = ControlPaint.Dark(Color1, 0.2f);
+            c2 = ControlPaint.Dark(Color2, 0.2f);
+        }
+
+        using (LinearGradientBrush brush =
+            new LinearGradientBrush(rect, c1, c2, 0f))
+        {
+            g.FillRectangle(brush, rect);
+        }
+
+        // Draw text
+        TextRenderer.DrawText(
+            g,
+            this.Text,
+            this.Font,
+            rect,
+            this.ForeColor,
+            TextFormatFlags.HorizontalCenter |
+            TextFormatFlags.VerticalCenter);
+
+        // Disabled overlay
+        if (!this.Enabled)
+        {
+            using (SolidBrush overlay =
+                new SolidBrush(Color.FromArgb(120, Color.Black)))
+            {
+                g.FillRectangle(overlay, rect);
+            }
+        }
     }
-  }
 }
